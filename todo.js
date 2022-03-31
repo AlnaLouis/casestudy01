5303e69f5ce40499abdf92f83d486dd96f7e453a
AlnaLouis <alnacl2001@gmail.com>
2022-03-31 23:55:48 +0530

    update

--- todo.js
+++ todo.js
@@ -1,75 +1,48 @@
-//creating object
-function ajax()
-{
-        //creating an Xhr object
-        //step 1: 
-        var xhttp = new XMLHttpRequest();
-        //eventlistener
-        //step 4:
-        xhttp.onreadystatechange = function(){
-            //condition
-            //step 5:
-            if(this.readyState==4 && this.status==200){
-                 var response =JSON.parse(this.responseText);
-                 //go to mdn web docs for more information about states
-                //we will get object object as output while using parse
-                //if we dont use parse we will get json array in a line
-               
-                var output="";
-                for(var i = 0;i<response.length;i++)
-            {
-                    if(response[i].complete==false){
-                //we can insert <li>tad to make it beautiful
-                output +=`<ul class="list-group gy-2 w-24"><li class="list-group-item"><input class="form-check-input me-1" id="box" type="checkbox" value=""  onchange= "validater()" aria-label="...">`
-                +`&nbsp`+response[i].title+ `
-                 </li></ul><br>`;
-                    }
-                    else{
-                        output +=`<ul class="list-group gy-2 w-24"><li class="list-group-item"><input class="form-check-input me-1" id="box" type="checkbox" value=""  checked disabled aria-label="...">`
-                        +`&nbsp`+response[i].title+ `
-                         </li></ul><br>`;
-
-                    }
-
+function ajax(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState==4&&this.status==200){
+            var response = JSON.parse(this.responseText);
+            var output = "";
+            for(var i=0;i<response.length;i++){
+                if(response[i].completed==false){
+                    output += '<li class="list-group-item"> <input type="checkbox" class="form-check-input checkbox" onchange="validate()">' +'&nbsp'+  response[i].title + "<br>" ;  
+                }
+                else{
+                output += '<li class="list-group-item list-group-item-info"> <input type="checkbox" class="form-check-input" checked disabled>'+ '&nbsp'  + response[i].title + "<br>" ;
+                }
             }
-            
-            //step 6:
-            document.getElementById("demo").innerHTML=output;
+            document.getElementById("demo").innerHTML = output;
+        }
+    }
+    xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
+    xhttp.send();
+}
+
+function validate(){
+   var get = new Promise(function(resolve,reject){
+        var list = document.getElementsByClassName("checkbox");
+        var count =0;
+        for (var i=0; i<list.length; i++){
+            if (list[i].type == "checkbox" && list[i].checked == true){
+                count++;
             }
         }
-        //instatinating the request object
-        //step 2:
-xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
-//step 3:
-//sending request to server
-xhttp.send();
+        if(count%5==0 && count!=0 ){
+            resolve(`Congrats. ${count} Tasks have been Successfully Completed!!`);
+        }
+        else{
+            reject("5 Tasks are not completed");
+        }
+    })
+    get
+    .then(function(a){
+        alert(a);
+    })
+    .catch(function(b){
+        console.log(b);
+    });
 }
 
 
-
-function validater(){
-    var promise = new Promise(function(resolve,reject){
-         var chec = document.getElementsByClassName("box");
-         var num =0;
-         for (var i=0; i<list.length; i++){
-             if (chec[i].type == "box" && chec[i].checked == true){
-                 num++;
-             }
-         }
-         if(num%5==0 && num!=0 ){
-             resolve(`Congrats. ${num} Tasks have been Successfully Completed!!`);
-         }
-         else{
-             reject("5 Tasks are not completed");
-         }
-     })
-     get
-     .then(function(c){
-         alert(c);
-     })
-     .catch(function(s){
-         console.log(s);
-     });
-    }
-        
 
