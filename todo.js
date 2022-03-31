591f950dbf9d0bed7a0bca2ed2094891d5e7c48c
AlnaLouis <alnacl2001@gmail.com>
2022-04-01 00:06:21 +0530

    update

--- todo.js
+++ todo.js
@@ -6,7 +6,7 @@ function ajax(){
             var output = "";
             for(var i=0;i<response.length;i++){
                 if(response[i].completed==false){
-                    output += '<li class="list-group-item"> <input type="checkbox" class="form-check-input checkbox" onchange="validate()">' +'&nbsp'+  response[i].title + "<br>" ;  
+                    output += '<li class="list-group-item"> <input type="checkbox" class="form-check-input checkbox" onchange="validater()">' +'&nbsp'+  response[i].title + "<br>" ;  
                 }
                 else{
                 output += '<li class="list-group-item list-group-item-info"> <input type="checkbox" class="form-check-input" checked disabled>'+ '&nbsp'  + response[i].title + "<br>" ;
@@ -19,28 +19,28 @@ function ajax(){
     xhttp.send();
 }
 
-function validate(){
-   var get = new Promise(function(resolve,reject){
-        var list = document.getElementsByClassName("checkbox");
-        var count =0;
-        for (var i=0; i<list.length; i++){
-            if (list[i].type == "checkbox" && list[i].checked == true){
-                count++;
+function validater(){
+   var promise = new Promise(function(resolve,reject){
+        var todo = document.getElementsByClassName("checkbox");
+        var num =0;
+        for (var i=0; i<todo.length; i++){
+            if (todo[i].type == "checkbox" && todo[i].checked == true){
+                num++;
             }
         }
-        if(count%5==0 && count!=0 ){
-            resolve(`Congrats. ${count} Tasks have been Successfully Completed!!`);
+        if(num%5==0 && num!=0 ){
+            resolve(`Congrats. ${num} Tasks have been Successfully Completed!!`);
         }
         else{
             reject("5 Tasks are not completed");
         }
     })
-    get
-    .then(function(a){
-        alert(a);
+    promise
+    .then(function(message){
+        alert(message);
     })
-    .catch(function(b){
-        console.log(b);
+    .catch(function(message){
+        console.log(message);
     });
 }
 
